c85a48da978ae50a376800f0c34171c8833a6c16
cloud-zhoubingye <zhou_bingye@outlook.com>
2025-05-22 10:27:22 +0800

    update code

---
+++ llmc/compression/quantization/utils.py
@@ -0,0 +1,32 @@
+import torch
+
+
+def make_divisible(c, divisor):
+    return (c + divisor - 1) // divisor
+
+
+def calculate_zeros_width(in_features, group_size=128, pack_num=8):
+    if group_size >= 128:
+        size_multiplier = 1
+    elif group_size == 64:
+        size_multiplier = 2
+    elif group_size == 32:
+        size_multiplier = 4
+    else:
+        raise NotImplementedError
+
+    base_width = make_divisible(in_features // group_size, pack_num)
+    base_width = make_divisible(base_width, size_multiplier) * size_multiplier
+    return base_width
+
+
+def is_fp8_supported_gpu():
+    if not torch.cuda.is_available():
+        return False
+    compute_capability = torch.cuda.get_device_capability(0)
+    major, minor = compute_capability
+    return (major == 8 and minor == 9) or (major >= 9)
+
+
+def ceil_div(x, y):
+    return (x + y - 1) // y
---
+++ llmc/eval/__init__.py
@@ -0,0 +1,7 @@
+from .eval_acc import AccuracyEval
+from .eval_code import HumanEval
+from .eval_custom_generate import CustomGenerate
+from .eval_ppl import DecodePerplexityEval, PerplexityEval
+from .eval_token_consist import TokenConsistencyEval
+from .eval_video_generate import VideoGenerateEval
+from .eval_vqa import VQAEval
