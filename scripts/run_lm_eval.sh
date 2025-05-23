e1760ecf7ab2bc0f3680ec6ea25197c70b740a89
cloud-zhoubingye <zhou_bingye@outlook.com>
2025-05-23 13:03:55 +0800

    change script name

--- scripts/run_lm_eval.sh
+++ scripts/run_lm_eval.sh
@@ -1,11 +1,11 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-llmc=./llmc
-lm_eval=./llmc/lm-evaluation-harness
+export CUDA_VISIBLE_DEVICES=0
+llmc=/kaggle/working/Awq/llmc
+lm_eval=/kaggle/working/Awq/llmc/lm-evaluation-harness
 export PYTHONPATH=$llmc:$PYTHONPATH
 export PYTHONPATH=$llmc:$lm_eval:$PYTHONPATH
 # Replace the config file (i.e., RTN with algorithm-transformed model path or notate quant with original model path) 
 # with the one you want to use. `--quarot` is depend on the transformation algorithm used before.
-accelerate launch --multi_gpu --num_processes 4 llmc/tools/llm_eval.py \
+python llmc/tools/llm_eval.py \
     --config llmc/configs/quantization/RTN/rtn_quarot.yml \
     --model hf \
     --quarot \
